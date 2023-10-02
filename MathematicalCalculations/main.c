8dd1706ae4609e9666a4d7e69034879a71acff9b
sideshowbobgot <sideshowbobgot@gmail.com>
2023-10-02 20:01:45 +0300

    mathematical_expectations: in progress

---
+++ MathematicalCalculations/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double calc_z_one(double alpha);
+double calc_z_two(double alpha);
+
+int main() {
+    char buf[255] = {0};
+
+    printf("Enter alpha: ");
+    scanf("%s", buf);
+
+    double alpha = strtod(buf, NULL);;
+
+    printf("z_1: %lf\n", calc_z_one(alpha));
+    printf("z_2: %lf\n", calc_z_two(alpha));
+
+    return 0;
+}
+
+double calc_z_one(double alpha) {
+    double z_one = 2;
+    z_one *= pow(sin(3 * M_PI - 2 * alpha), 2);
+    z_one *= pow(cos(5 * M_PI + 2 * alpha), 2);
+    return z_one;
+}
+
+double calc_z_two(double alpha) {
+    double z_two = 0.25;
+    z_two -= 0.25 * sin(2.5 * M_PI - 8 * alpha);
+    return z_two;
+}
