b77e8e766b765694da2f80d99eb629e739e5345c
sideshowbobgot <sideshowbobgot@gmail.com>
2023-12-28 19:03:55 +0200

    Fixed Lab7

--- Lab7/Core/Src/main.c
+++ Lab7/Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "MY_LIS3DSH.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -43,7 +43,8 @@
 SPI_HandleTypeDef hspi1;
 
 /* USER CODE BEGIN PV */
-
+LIS3DSH_DataScaled myData;
+uint8_t drdyFlag=0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -56,6 +57,16 @@ static void MX_SPI1_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+  /* Prevent unused argument(s) compilation warning */
+  UNUSED(GPIO_Pin);
+  /* NOTE: This function Should not be modified, when the callback is needed,
+           the HAL_GPIO_EXTI_Callback could be implemented in the user file
+   */
+
+  drdyFlag = 1;
+}
 
 /* USER CODE END 0 */
 
@@ -89,6 +100,18 @@ int main(void)
   MX_GPIO_Init();
   MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
+  LIS3DSH_InitTypeDef myAccConfigDef;
+
+    myAccConfigDef.dataRate = LIS3DSH_DATARATE_25;
+    myAccConfigDef.fullScale = LIS3DSH_FULLSCALE_4;
+    myAccConfigDef.antiAliasingBW = LIS3DSH_FILTER_BW_50;
+    myAccConfigDef.enableAxes = LIS3DSH_XYZ_ENABLE;
+    myAccConfigDef.interruptEnable = true;
+    LIS3DSH_Init(&hspi1, &myAccConfigDef);
+
+    LIS3DSH_X_calibrate(-1000.0, 980.0);
+    LIS3DSH_Y_calibrate(-1020.0, 1040.0);
+    LIS3DSH_Z_calibrate(-920.0, 1040.0);
 
   /* USER CODE END 2 */
 
@@ -96,6 +119,36 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  if(drdyFlag == 1)
+	  	  {
+	  		  myData = LIS3DSH_GetDataScaled();
+	  		  if(myData.x<-50){
+	  			  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
+	  			  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+	  		  }
+	  		  else if(myData.x>50){
+	  			  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+	  			  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+	  			  }
+	  		  else {
+	  			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+	  			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+	  		  }
+
+	  		if(myData.y<-50){
+	  			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
+	  			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+	  		}
+	  		else if(myData.y>50){
+	  			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+	  			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+	  			  			  }
+	  		else {
+	  			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+	  			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+	  			}
+	  		  drdyFlag = 0;
+	  	  }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
